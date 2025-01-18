71afb99d74cb356cdb97c2388dd60cc5be5e4ddc
louloveai <viethoangvo5@gmail.com>
2025-01-18 10:10:43 +0700

    scraper.py

---
+++ data_collection/scraper.py
@@ -0,0 +1,17 @@
+class DataScraper:
+    def __init__(self):
+        self.sources = {
+            'forums': ['webtretho.com', 'voz.vn'],
+            'blogs': ['tamly.blog', 'psychologytoday.com'],
+            'social': ['facebook_groups', 'reddit.com/r/psychology']
+        }
+        
+    def scrape_conversations(self):
+        """Thu thập các cuộc trò chuyện từ forums"""
+        # Tự động crawl các bài viết về tâm lý, mental health
+        pass
+        
+    def extract_patterns(self, text):
+        """Tự động nhận diện patterns từ text"""
+        # Dùng NLP để phát hiện cấu trúc câu, emotion
+        pass 
